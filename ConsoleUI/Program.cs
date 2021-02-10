02c6ba0ebf03ad8515adb3b00bb63ca338db083d
Deniz Dinler <dinlerd@hotmail.com>
2021-02-11 00:19:36 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,49 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        //SOLID
+        //Open Closed Principle
+        static void Main(string[] args)
+        {
+            //Data Transformation Object
+            ProductTest();
+            //IoC
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            var result = productManager.GetProductDetails();
+            if (result.Success)
+            {
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+        }
+    }
+}
---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Category:IEntity
+    {
+        //Çıplak Class Kalmasın
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
