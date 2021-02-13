e4753e46dc98d8661990e7452587a19c77bf1341
Deniz Dinler <dinlerd@hotmail.com>
2021-02-13 23:57:53 +0300

    Update 13022021

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -36,7 +36,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour==00)
+            if (DateTime.Now.Hour==22)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
